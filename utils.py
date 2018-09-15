d46fe7578498444fe17642f828586a7be99d8b42
Tae Hyung Kim <thkim1011@berkeley.edu>
2018-09-15 11:28:48 -0400

    Initial Commit

---
+++ utils.py
@@ -0,0 +1,7 @@
+def compute_positions(accel_x, accel_y):
+    """
+    Given acceleration data accel_x and accel_y,
+    compute the relative positions of the "particle"
+    >>> accel_x = [1, 2, 3]
+    >>> accel_y
+    """
