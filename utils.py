2a7f2da94d49989ac2fb29b291f01d39aa47a9dc
Tae Hyung Kim <thkim1011@berkeley.edu>
2018-09-15 12:07:47 -0400

    Done (?) with utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,36 @@
-def compute_positions(accel_x, accel_y):
+import numpy
+
+def compute_linear_pos(accel, time):
+    """
+    Given acceleration data accel and time,
+    compute the relative position at each time.
+    Takes in lists of length n and outputs pos list
+    of length n + 1.
+    """
+    accel = np.array(accel)
+    time = np.array(time)
+    return np.cumsum(0.5 * accel * time**2)
+
+def compute_positions(accel_x, accel_y, time):
     """
     Given acceleration data accel_x and accel_y,
-    compute the relative positions of the "particle"
-    >>> accel_x = [1, 2, 3]
-    >>> accel_y
+    compute the relative positions of the "particle".
+    In addition use signal processing to smooth out. 
     """
+    return compute_linear_pos(accel_x, time),
+            compute_linear_pos(accel_y, time)
+
+def calibrate_gps(pos_x, pos_y, gps_x, gps_y, time):
+    """
+    Given relative positions and gps data at each point
+    compute the best initial position which fits the data.
+    """
+    # Compute initial position
+    pos_x = np.array(pos_x)
+    pos_y = np.array(pos_y)
+    gps_x = np.array(gps_x)
+    gps_y = np.array(gps_y)
+    initial = (np.sum(gps_x - pos_x)/len(gps_x),
+            np.sum(gps_y - pos_y)/len(gps_y))
+    return pos_x + initial[0], pos_y + initial[1]
+
