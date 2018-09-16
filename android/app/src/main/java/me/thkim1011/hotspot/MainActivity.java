c8d99a6738e4bff813033466c63d85f74d7727fb
Tae Hyung Kim <thkim1011@berkeley.edu>
2018-09-16 10:50:32 -0400

    asdf

--- android/app/src/main/java/me/thkim1011/hotspot/MainActivity.java
+++ android/app/src/main/java/me/thkim1011/hotspot/MainActivity.java
@@ -1,32 +1,19 @@
 package me.thkim1011.hotspot;
 
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
-
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.firebase.database.DatabaseReference;
-
-import java.io.FileWriter;
-import java.io.IOException;
-
 
 public class MainActivity extends AppCompatActivity {
     private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
-        mWebView = (WebView) findViewById(R.id.web);
+        super.onCreate(savedInstanceState);
+        mWebView = (WebView) this.findViewById(R.id.webview);
         mWebView.loadUrl("http://google.com");
 
-        setContentView(R.layout.activity_main);
+        setContentView(mWebView);
     }
 }
